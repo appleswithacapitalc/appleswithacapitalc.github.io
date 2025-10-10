d33033f76df8ac90a7f768c22b237ca158aafe89
Mian Wei <mianwei8@gmail.com>
2025-10-10 04:36:41 -0400

    tweaked easter egg

---
+++ mera/assets/js/c2b.js
@@ -0,0 +1,5 @@
+ (function ($) {
+  $(document).ready(function() {
+                    $(".image-container").css("height", $(".embed-responsive-item").height());
+                    });
+  })(jQuery);
