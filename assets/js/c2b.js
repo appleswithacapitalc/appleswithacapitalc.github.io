e3847448a40b67cf7cfdfdc87f5f8c67470efde5
Mian Wei <mianwei8@gmail.com>
2025-10-10 04:40:16 -0400

    removed extraneous dirs

--- assets/js/c2b.js
+++
@@ -1,5 +0,0 @@
- (function ($) {
-  $(document).ready(function() {
-                    $(".image-container").css("height", $(".embed-responsive-item").height());
-                    });
-  })(jQuery);
